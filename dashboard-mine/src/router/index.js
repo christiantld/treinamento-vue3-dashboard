34e1f26e3d48e7fdd9369da1feec0d01f4a2f505
Christian Toledo <chrstiantoledo@gmail.com>
2021-01-22 21:19:54 -0300

    project setup

---
+++ dashboard-mine/src/router/index.js
@@ -0,0 +1,44 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+// const Feedbacks = defineAsyncComponent({
+//   loader: () => import('')
+// })
+
+const Feedbacks = () => import('@/views/Feedbacks/index.vue')
+const Credentials = () => import('@/views/Credentials/index.vue')
+const Home = () => import('@/views/Home/index.vue')
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/feedbacks',
+    name: 'Feedbacks',
+    component: Feedbacks,
+    meta: {
+      hasAuth: true
+    }
+  },
+  {
+    path: '/credentials',
+    name: 'Credentials',
+    component: Credentials,
+    meta: {
+      hasAuth: true
+    }
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    redirect: { name: 'Home' }
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory('/'),
+  routes
+})
+
+export default router
