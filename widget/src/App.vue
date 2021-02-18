16b7dc6391cece8804bd8c41e0c27ec77383b497
Christian Toledo <chrstiantoledo@gmail.com>
2021-02-18 19:31:59 -0300

    project setup

---
+++ widget/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>App</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'App'
+})
+</script>
---
+++ widget/src/main.ts
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import 'animate.css'
+import '@/assets/css/tailwind.css'
+import '@/assets/css/fonts.css'
+
+const app = createApp(App)
+app.mount('#app')
