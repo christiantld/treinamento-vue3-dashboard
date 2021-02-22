9b9180986c0ef96110355c59bdcf7327ce3606ad
Christian Toledo <chrstiantoledo@gmail.com>
2021-02-22 14:54:00 -0300

    add store, playground and bootstrap modules

--- widget/src/main.ts
+++ widget/src/main.ts
@@ -1,9 +1,20 @@
 import { createApp } from 'vue'
+import Playground from '@/views/Playground/index.vue'
 import App from './App.vue'
+
+import { setup } from '@/utils/bootstrap'
 
 import 'animate.css'
 import '@/assets/css/tailwind.css'
 import '@/assets/css/fonts.css'
 
-const app = createApp(App)
-app.mount('#app')
+setup({
+  onProduction: () => {
+    const app = createApp(App)
+    app.mount('#app')
+  },
+  onDevelopment: () => {
+    const app = createApp(Playground)
+    app.mount('#app')
+  }
+})
