86e6c0574b896b750abd62c22be10e45132fc003
Christian Toledo <chrstiantoledo@gmail.com>
2021-03-03 14:45:57 -0300

    Add wizard component

---
+++ widget/src/components/Wizard/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <component
+    :is="store.currentComponent"
+    v-bind="$props"
+    @select-feedback-type="handleSelectFeedbackType"
+    @next="next"
+  />
+</template>
+
+<script lang="ts">
+import useNavigation, { Navigation } from '@/hooks/navigation'
+import useStore from '@/hooks/store'
+import { setFeedbackType, StoreState } from '@/store'
+import { defineComponent } from 'vue'
+import SelectFeedbackType from './SelectFeedbackType.vue'
+import WriteAFeedback from './WriteAFeedback.vue'
+import Success from './Success.vue'
+
+interface SetupReturn {
+  store: StoreState
+  handleSelectFeedbackType(type: string): void
+  next: Navigation['next']
+}
+
+export default defineComponent({
+  components: { SelectFeedbackType, WriteAFeedback, Success },
+  setup(): SetupReturn {
+    const store = useStore()
+    const { next } = useNavigation()
+    function handleSelectFeedbackType(type: string): void {
+      setFeedbackType(type)
+    }
+    return { store, handleSelectFeedbackType, next }
+  }
+})
+</script>
+
+<style>
+</style>
