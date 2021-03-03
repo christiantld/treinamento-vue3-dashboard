8b93ad97b107c4b1b4a915806fa8c03d0a68232e
Christian Toledo <chrstiantoledo@gmail.com>
2021-03-03 14:43:45 -0300

    Add Icons

--- widget/src/components/Icon/index.vue
+++ widget/src/components/Icon/index.vue
@@ -7,9 +7,22 @@ import Loading from './Loading.vue'
 import Copy from './Copy.vue'
 import ArrowDown from './ArrowDown'
 import Chat from './Chat'
+import ArrowRight from './ArrowRight'
+import Close from './Close'
+import Check from './Check.vue'
+import Atention from './Atention.vue'
 
 export default {
-  components: { Loading, Copy, ArrowDown, Chat },
+  components: {
+    Loading,
+    Copy,
+    ArrowDown,
+    Chat,
+    Close,
+    ArrowRight,
+    Check,
+    Atention
+  },
   props: {
     name: { type: String, required: true }
   }
